62fd90a3ba10df2a2f6b2128096a4c757d42402a
Rachel Charow <rachel.charow@gmail.com>
2015-07-16 17:09:34 -0400

    update port

--- start.js
+++ start.js
@@ -2,7 +2,7 @@
 // Grabbing our server from our server/index.js file.
 var server = require('./server');
 
-var PORT = 1337;
+var PORT = process.env.PORT || 1337;
 
 server.listen(PORT, function () {
     console.log('Server started on port ' + PORT.toString());
